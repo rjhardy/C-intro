32f0734e3175827581347689de81dedeefee07f7
hardyrd <hardyrd@users.noreply.github.uc.edu>
2019-02-10 21:34:56 -0500

    Add files via upload

--- lab4/field.cpp
+++ lab4/field.cpp
@@ -1,5 +1,28 @@
-// placeholder file
-// place your code here
-//
-//
-// See the google docs for details
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+double Efield(double r, double a = 1)   {
+    if (r < a){
+        double Efield = 9 * (r / pow(a, 3));
+        return Efield;
+    }
+    else if (r >= a)   {
+        double Efield = 9 / pow(r, 2);
+        return Efield;
+    }
+}
+
+int main()   {
+
+    int r;
+    int a;
+
+    cin >> r;
+
+    cin >> a;
+    cout << "Efield: " << Efield(r, a) << endl;
+
+    return 0;
+}
--- lab4/newton_raphson.cpp
+++ lab4/newton_raphson.cpp
@@ -1,6 +1,51 @@
-// placeholder file
-// place your code here
-//
-//
-// See the google docs for details
+#include <iostream>
+#include <cmath>
+#include <iomanip>
 
+using namespace std;
+
+const double TOLERANCE = .001;
+
+double f(double nth)   {
+    double fx;
+
+    fx = pow(nth,4) + (2*(pow(nth,3))) - (31*(pow(nth,2))) - (32 * nth) + 60;
+
+    return fx;
+}
+
+double fprime(double nth)   {
+    double dfx;
+
+    dfx = (4 * pow(nth,3)) + (6 * pow(nth,2)) - (62 * nth) - 32;
+
+    return dfx;
+}
+
+double newtonRoot(double x0)   {
+    double xone;
+
+    xone = x0 - (f(x0)/fprime(x0));
+    double tol = abs(xone - x0);
+
+    while(tol > TOLERANCE)   {
+        x0 = xone;
+        xone = x0 - (f(x0)/fprime(x0));
+        tol = abs(xone - x0);
+    }
+    return xone;
+}
+
+int main()   {
+    double guess;
+    char go = 'y';
+    do{
+        cout << "Enter a guess" << endl;
+        cin >> guess;
+        cout << "Root: " << newtonRoot(guess) << endl;
+        cout << "Enter Another Guess: y/n?";
+        cin >> go;
+    }while(go == 'y');
+
+    return 0;
+}
